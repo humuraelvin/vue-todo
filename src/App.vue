d8a7150395a8c545636a40f82ba1af8f6987b90b
HUMURA ELVIN <humura53@gmail.com>
2024-05-28 07:17:02 +0200

    changes made

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted, computed, watch } from "vue";
+</script>
 
-<template></template>
+const todos = ref([]);
+const name = ref('');
+const input_content = ref('');
+const input_category = ref(null); const todo_asc = computed(() =>
+todos.value.sort((a,b) => {
+   return a.createdAt - b.createdAt 
+}))
 
- 
-
+<template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        Whats up,
+        <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
+  </main>
+</template>
